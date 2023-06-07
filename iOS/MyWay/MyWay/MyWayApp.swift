c237f4c7feb6e36856619ce7a1f4e2908dddf31c
Eunbin Kwon <kwoneunbini@gmail.com>
2023-06-07 21:44:43 +0900

    [feat] Initial Commit - iOS in SwiftUI

---
+++ iOS/MyWay/MyWay/MyWayApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWayApp.swift
+//  MyWay
+//
+//  Created by 권은빈 on 2023/06/07.
+//
+
+import SwiftUI
+
+@main
+struct MyWayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
